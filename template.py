49d808172e8cec6f075a68655e7f44708a2384f3
0-0-00-0 <guozy@shanghaitech.edu.cn>
2023-05-21 00:03:53 +0800

    控制taxi位置，乘客位置，目的地

--- template.py
+++ template.py
@@ -4,13 +4,16 @@ env = gym.make("Taxi-v3", render_mode="human")
 '''
 template random taxi agent
 '''
-observation, info = env.reset()
+for __ in range(5):
+    state=env.encode(4,4,0,1) # taxi row 0-4, taxi col 0-4, passgr loc 0-3, passgr dest 0-3
+    observation, info = env.reset(state=state)
 
-for _ in range(1000):
-    action = env.action_space.sample()  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = env.step(action)
+    for _ in range(2):
+        action = env.action_space.sample()  # agent policy that uses the observation and info
+        print(action)
+        observation, reward, terminated, truncated, info = env.step(action)
 
-    if terminated or truncated:
-        observation, info = env.reset()
+        if terminated or truncated:
+            observation, info = env.reset()
 
 env.close()
