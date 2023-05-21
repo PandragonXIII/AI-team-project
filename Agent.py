924b700225233d924bc4c6440c2f1186ed63b337
qxy <qxy@nomail.com>
2023-05-21 12:09:06 +0800

    Added fog and modified corresponding functions

--- Agent.py
+++ Agent.py
@@ -1,9 +1,27 @@
 import numpy as np
 
+
+def AddFrogToObs(env, observation:list, visible_dis : int =2)->list:
+    """
+    passenger is visible only if it is within visible_dis
+    return (taxi_row, taxi_col, passenger_location(None), destination(None))
+    """
+    taxi_row, taxi_col, passenger_location, destination = observation
+    if passenger_location == 4: # passenger in taxi
+        return observation
+    else:
+        passenger_row, passenger_col = env.locs[passenger_location]
+        if abs(taxi_row - passenger_row) <= visible_dis and abs(taxi_col - passenger_col) <= visible_dis: #square visual
+            return observation
+        else:
+            return [taxi_row, taxi_col, None, None]
+        
+
 class Agent(object):
-    def __init__(self, observation_space, action_space):
-        self.observation_space = observation_space
-        self.action_space = action_space
+    def __init__(self, env):
+        self.observation_space = env.observation_space
+        self.action_space = env.action_space
+        self.env = env
         return 
     
     def explore(self, observation):
@@ -16,8 +34,8 @@ class Agent(object):
 
 
 class RandomAgent(Agent):
-    def __init__(self, observation_space, action_space):
-        super(RandomAgent, self).__init__(observation_space, action_space)
+    def __init__(self, env):
+        super(RandomAgent, self).__init__(env=env)
         return
     
     def explore(self, observation):
@@ -28,9 +46,9 @@ class RandomAgent(Agent):
 
 
 class ReinforcementAgent(Agent):
-    def __init__(self, observation_space, action_space,
+    def __init__(self, env,
                  learning_rate=0.1, discount_factor=0.9, explore = 0.1):
-        super(ReinforcementAgent, self).__init__(observation_space, action_space)
+        super(ReinforcementAgent, self).__init__(env)
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
         self.explore_constant = explore
@@ -40,14 +58,35 @@ class ReinforcementAgent(Agent):
         self.q_table[:,:,1] = 1
         return 
     
-    def explore(self, observation)->int:
+    def explore(self, observation:int|list)->int:
+        #if observation is a tuple
+        if not isinstance(observation, int):
+            # print(observation)
+            if None in observation:
+                raise Exception("invisible passenger")
+            else: # all locs are valid
+                taxi_row, taxi_col, passenger_location, destination = observation
+                observation = self.env.encode(taxi_row, taxi_col, passenger_location, destination)
+        # do as usual (int)
         action = self.get_best_action(observation)
         self.q_table[observation, action, 1] += 1
         return action
     
-    def update(self, old_observation, action, observation, reward):
+    def update(self, old_observation:int|list, action, observation:int|list, reward):
         # Exploration Function
         # give the less visited action an additional weight
+        if not isinstance(observation, int): #if observation is a list
+            if None in observation:
+                raise Exception("invisible passenger")
+            else:
+                taxi_row, taxi_col, passenger_location, destination = observation
+                observation = self.env.encode(taxi_row, taxi_col, passenger_location, destination)
+        if not isinstance(old_observation, int): #if observation is a list
+            if None in old_observation:
+                raise Exception("invisible passenger")
+            else:
+                taxi_row, taxi_col, passenger_location, destination = old_observation
+                old_observation = self.env.encode(taxi_row, taxi_col, passenger_location, destination)
         next_actions = self.q_table[observation]
         next_actions = next_actions[:,0]+self.explore_constant/next_actions[:,1]
         temp = reward+self.discount_factor*np.max(next_actions)
@@ -56,6 +95,13 @@ class ReinforcementAgent(Agent):
             + self.learning_rate*temp
         return
 
-    def get_best_action(self, observation):
+    def get_best_action(self, observation:int|list):
         # find the action with max q value in present state
+        #if observation is iterable 
+        if not isinstance(observation, int):
+            if None in observation:
+                raise Exception("invisible passenger")
+            else:
+                taxi_row, taxi_col, passenger_location, destination = observation
+                observation = self.env.encode(taxi_row, taxi_col, passenger_location, destination)
         return np.argmax(self.q_table[observation, :, 0])
--- main.py
+++ main.py
@@ -28,19 +28,20 @@ env = gym.make("Taxi-v3")
 AGENT_TYPE = "reinforcement"
 test_times = 10
 display_times = 10
+FROG_OF_WAR = False
 
 """
 note that we only have one passenger in each episode for now.
 车的行为是确定性的, 需要学习的应该是墙的位置和乘客的位置
 """
 if AGENT_TYPE == "random":
-    agent = Agent.RandomAgent(env.observation_space, env.action_space)
+    agent = Agent.RandomAgent(env)
     train_times = 0 # random agent does not need training
 elif AGENT_TYPE == "reinforcement":
-    agent = Agent.ReinforcementAgent(env.observation_space, env.action_space,
-                                     learning_rate=0.05, discount_factor=0.99,
+    agent = Agent.ReinforcementAgent(env,
+                                     learning_rate=0.1, discount_factor=0.99,
                                      explore=1)
-    train_times = 5000
+    train_times = 3000
 else:
     raise Exception("unknown agent type")
 
@@ -49,23 +50,34 @@ print("-----training-----")
 for _ in range(train_times):
     print(".", end="")
     observation, info = env.reset()
+    observation = list(env.decode(observation))
+    """
+    ATTENTION!
+    observation = [taxi_row, taxi_col, passenger_location, destination]
+    observation is always list now, 除非是在函数中作为index使用
+    """
     terminated, truncated = False, False
+    if FROG_OF_WAR:
+        observation = Agent.AddFrogToObs(env, observation, visible_dis=2)        
     while not( terminated or truncated):
         action = agent.explore(observation)
         old_observation = observation
         observation, reward, terminated, truncated, info = env.step(action)
+        observation = list(env.decode(observation))
         agent.update(old_observation, action, observation, reward)
 print("\n")
 
 # start testing
 for _ in range(test_times):
     observation, info = env.reset()
+    observation = list(env.decode(observation))
     terminated, truncated = False, False
     print("-----test:{}-----".format(_))
     total_reward = 0
     while not( terminated or truncated):
         action = agent.get_best_action(observation)
         observation, reward, terminated, truncated, info = env.step(action)
+        observation = list(env.decode(observation))
         total_reward += reward
     # game will terminate automatically after 200 steps
     # print the final score
@@ -77,11 +89,13 @@ env = gym.make("Taxi-v3", render_mode="human")
 print("-----display-----")
 for _ in range(display_times):
     observation, info = env.reset()
+    observation = list(env.decode(observation))
     terminated, truncated = False, False
     rewards = 0
     for s in range(30):
         action = agent.get_best_action(observation)
         new_observation, reward, terminated, truncated, info = env.step(action)
+        new_observation = list(env.decode(new_observation))
         rewards += reward
         # env.render()
         observation = new_observation
