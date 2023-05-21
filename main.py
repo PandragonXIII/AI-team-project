1288b05b0c390ecc58f345fe813c5e8a38c5ff52
qxy <qxy@nomail.com>
2023-05-21 12:19:54 +0800

    updated readme and main

--- main.py
+++ main.py
@@ -24,86 +24,90 @@ Rewards:
     -10 executing “pickup” and “drop-off” actions illegally.
 
 '''
-env = gym.make("Taxi-v3")
-AGENT_TYPE = "search"
-test_times = 10
-display_times = 10
-FROG_OF_WAR = False
+def main():
+    env = gym.make("Taxi-v3")
+    AGENT_TYPE = "search"
+    test_times = 10
+    display_times = 10
+    FROG_OF_WAR = False
 
-"""
-note that we only have one passenger in each episode for now.
-车的行为是确定性的, 需要学习的应该是墙的位置和乘客的位置
-"""
-if AGENT_TYPE == "random":
-    agent = Agent.RandomAgent(env)
-    train_times = 0 # random agent does not need training
-elif AGENT_TYPE == "reinforcement":
-    agent = Agent.ReinforcementAgent(env,
-                                     learning_rate=0.1, discount_factor=0.99,
-                                     explore=1)
-    train_times = 3000
-elif AGENT_TYPE == "search":
-    agent = Agent.SearchAgent(env)
-    train_times = 0
-else:
-    raise Exception("unknown agent type")
-
-# start training
-print("-----training-----")
-for _ in range(train_times):
-    print(".", end="")
-    observation, info = env.reset()
-    observation = list(env.decode(observation))
     """
-    ATTENTION!
-    observation = [taxi_row, taxi_col, passenger_location, destination]
-    observation is always list now, 除非是在函数中作为index使用
+    note that we only have one passenger in each episode for now.
+    车的行为是确定性的, 需要学习的应该是墙的位置和乘客的位置
     """
-    terminated, truncated = False, False
-    if FROG_OF_WAR:
-        observation = Agent.AddFrogToObs(env, observation, visible_dis=2)        
-    while not( terminated or truncated):
-        action = agent.explore(observation)
-        old_observation = observation
-        observation, reward, terminated, truncated, info = env.step(action)
+    if AGENT_TYPE == "random":
+        agent = Agent.RandomAgent(env)
+        train_times = 0 # random agent does not need training
+    elif AGENT_TYPE == "reinforcement":
+        agent = Agent.ReinforcementAgent(env,
+                                        learning_rate=0.1, discount_factor=0.99,
+                                        explore=1)
+        train_times = 3000
+    elif AGENT_TYPE == "search":
+        agent = Agent.SearchAgent(env)
+        train_times = 0
+    else:
+        raise Exception("unknown agent type")
+
+    # start training
+    print("-----training-----")
+    for _ in range(train_times):
+        print(".", end="")
+        observation, info = env.reset()
         observation = list(env.decode(observation))
-        agent.update(old_observation, action, observation, reward)
-print("\n")
+        """
+        ATTENTION!
+        observation = [taxi_row, taxi_col, passenger_location, destination]
+        observation is always list now, 除非是在函数中作为index使用
+        """
+        terminated, truncated = False, False
+        if FROG_OF_WAR:
+            observation = Agent.AddFrogToObs(env, observation, visible_dis=2)        
+        while not( terminated or truncated):
+            action = agent.explore(observation)
+            old_observation = observation
+            observation, reward, terminated, truncated, info = env.step(action)
+            observation = list(env.decode(observation))
+            agent.update(old_observation, action, observation, reward)
+    print("\n")
 
-# start testing
-for _ in range(test_times):
-    observation, info = env.reset()
-    observation = list(env.decode(observation))
-    terminated, truncated = False, False
-    print("-----test:{}-----".format(_))
-    total_reward = 0
-    while not( terminated or truncated):
-        action = agent.get_best_action(observation)
-        observation, reward, terminated, truncated, info = env.step(action)
+    # start testing
+    for _ in range(test_times):
+        observation, info = env.reset()
         observation = list(env.decode(observation))
-        total_reward += reward
-    # game will terminate automatically after 200 steps
-    # print the final score
-    print("score: ", total_reward)
-env.close()
+        terminated, truncated = False, False
+        print("-----test:{}-----".format(_))
+        total_reward = 0
+        while not( terminated or truncated):
+            action = agent.get_best_action(observation)
+            observation, reward, terminated, truncated, info = env.step(action)
+            observation = list(env.decode(observation))
+            total_reward += reward
+        # game will terminate automatically after 200 steps
+        # print the final score
+        print("score: ", total_reward)
+    env.close()
 
-env = gym.make("Taxi-v3", render_mode="human")
-# display
-print("-----display-----")
-for _ in range(display_times):
-    observation, info = env.reset()
-    observation = list(env.decode(observation))
-    terminated, truncated = False, False
-    rewards = 0
-    for s in range(30):
-        action = agent.get_best_action(observation)
-        new_observation, reward, terminated, truncated, info = env.step(action)
-        new_observation = list(env.decode(new_observation))
-        rewards += reward
-        # env.render()
-        observation = new_observation
+    env = gym.make("Taxi-v3", render_mode="human")
+    # display
+    print("-----display-----")
+    for _ in range(display_times):
+        observation, info = env.reset()
+        observation = list(env.decode(observation))
+        terminated, truncated = False, False
+        rewards = 0
+        for s in range(30):
+            action = agent.get_best_action(observation)
+            new_observation, reward, terminated, truncated, info = env.step(action)
+            new_observation = list(env.decode(new_observation))
+            rewards += reward
+            # env.render()
+            observation = new_observation
 
-        if terminated or truncated:
-            break
+            if terminated or truncated:
+                break
 
-env.close()
+    env.close()
+
+if __name__ == "__main__":
+    main()
