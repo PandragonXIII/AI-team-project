1a67b02520bb3133acf25e1a0674d141096671dc
qxy <qxy@nomail.com>
2023-05-22 15:28:37 +0800

    ReLU

--- statics_visual.py
+++ statics_visual.py
@@ -6,13 +6,17 @@ import numpy as np
 import main
 import gymnasium as gym
 
+
+def ReLU(x):
+    return np.maximum(0, x)
+
 def show_performance():
     AGENT_TYPE = "reinforcement"
     test_times = 50
     FROG_OF_WAR = False
-    train_time_range = [800,4000,400]
-    l_rate = [0.15, 0.71]
-    d_factor = 0.99
+    train_time_range = [4400,5000,400]
+    l_rate = [0.1, 0.4]
+    d_factor = 0.9
     expl = 1
 
     env = gym.make("Taxi-v3")
@@ -28,10 +32,10 @@ def show_performance():
                     learning_rate=l_rate, discount_factor=d_factor, explore=expl)
         for idx in range(len(x_data)-1):
             train_times = x_data[idx+1]-x_data[idx]
-            print("train times: ", train_times)
-            scores = main.single_test(AGENT_TYPE,test_times,FROG_OF_WAR,train_times,
-                                      l_rate,d_factor,expl, mute=True, agent=agent)
-            avg_score = np.exp2(np.mean(scores))
+            # train_times = x_data[idx+1]
+            print("train times: ", x_data[idx+1])
+            scores = main.single_test(AGENT_TYPE,agent,True,train_times,test_times)
+            avg_score = ReLU(np.mean(scores))
             z_data.append(avg_score)
     # 绘制热力图
     x_data = x_data[1:]
@@ -47,15 +51,36 @@ def show_performance():
 
     # note the score of search agent
     agent = main.Agent.SearchAgent(env=env)
-    scores = main.single_test("search",test_times,FROG_OF_WAR,0,
-                                l_rate,d_factor,expl, mute=True, agent=agent)
-    avg_score = np.exp2(np.mean(scores))
+    train_times = 0
+    scores = main.single_test('search',None,True,train_times,test_times,
+                                False ,FROG_OF_WAR,l_rate,d_factor,expl)
+    avg_score = ReLU(np.mean(scores))
     print("search agent score: ", avg_score)
     plt.show()
 
     return
 
-show_performance()
+# show_performance()
+
+def single_rein_tests():
+    AGENT_TYPE = "reinforcement"
+    test_times = 50
+    FROG_OF_WAR = False
+    train_times = 4000
+    l_rate = 0.02
+    d_factor = 0.99
+    expl = 1
+
+    env = gym.make("Taxi-v3")
+    agent = main.Agent.ReinforcementAgent(env=env,
+                    learning_rate=l_rate, discount_factor=d_factor, explore=expl)
+    scores = main.single_test(AGENT_TYPE,agent,True,train_times,test_times)
+    avg_score = ReLU(np.mean(scores))
+    print("scores: ", scores)
+    print("average score: ", avg_score)
+    return
+
+single_rein_tests()
 
 def comp_different_agents():
     test_times = 100
@@ -68,8 +93,9 @@ def comp_different_agents():
     x_data = ["random", "reinforcement", "search"]
     y_data = []
     for agent_type in x_data:
-        scores = main.single_test(agent_type,test_times,FROG_OF_WAR,train_times,l_rate,d_factor,expl, mute=True)
-        avg_score = np.exp2(np.mean(scores))
+        scores = main.single_test(agent_type,None,True,train_times,test_times,
+                                  False ,FROG_OF_WAR,l_rate,d_factor,expl)
+        avg_score = ReLU(np.mean(scores))
         y_data.append(avg_score)
     print(y_data)
     # 绘制bar图
