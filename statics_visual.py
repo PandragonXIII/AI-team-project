7a349917841fc3bc86f686cfe96566d174985170
qxy <qxy@nomail.com>
2023-05-22 22:35:32 +0800

    added big map test

--- statics_visual.py
+++ statics_visual.py
@@ -14,8 +14,8 @@ def show_performance():
     AGENT_TYPE = "reinforcement"
     test_times = 100
     FROG_OF_WAR = False
-    train_time_range = [800,4000,400]
-    l_rate = [0.1, 1, 0.05]
+    train_time_range = [1800,18000,600]
+    l_rate = [0.05, 1, 0.05]
     d_factor = 0.9
     expl = 1
 
@@ -35,7 +35,8 @@ def show_performance():
             # train_times = x_data[idx+1]
             print("train times: ", x_data[idx+1])
             scores = main.single_test(AGENT_TYPE,agent,True,train_times,test_times)
-            avg_score = ReLU(np.mean(scores))
+            avg_score = np.mean(scores)
+            print("score: ", avg_score)
             z_data.append(avg_score)
     # 绘制热力图
     x_data = x_data[1:]
@@ -50,23 +51,26 @@ def show_performance():
     # plt.show()
 
     # note the score of search agent
-    agent = main.Agent.SearchAgent(env=env)
-    train_times = 0
-    scores = main.single_test('search',None,True,train_times,test_times,
-                                False ,FROG_OF_WAR,l_rate,d_factor,expl)
-    avg_score = ReLU(np.mean(scores))
-    print("search agent score: ", avg_score)
+    # agent = main.Agent.SearchAgent(env=env)
+    # train_times = 0
+    # scores = main.single_test('search',None,True,train_times,test_times,
+    #                             False ,FROG_OF_WAR,l_rate,d_factor,expl)
+    # avg_score = ReLU(np.mean(scores))
+    # print("search agent score: ", avg_score)
     plt.show()
 
+    #store z_data in file
+    np.savetxt("z_data.txt", z_data, fmt="%f", delimiter=",")
+    
     return
 
-show_performance()
+# show_performance()
 
 def single_rein_tests():
     AGENT_TYPE = "reinforcement"
     test_times = 50
     FROG_OF_WAR = False
-    train_times = 4000
+    train_times = 7000
     l_rate = 0.1
     d_factor = 0.99
     expl = 1
@@ -85,7 +89,7 @@ def single_rein_tests():
 def comp_different_agents():
     test_times = 100
     FROG_OF_WAR = False
-    train_times = 4000
+    train_times = 7000
     l_rate = 0.1
     d_factor = 0.99
     expl = 1
@@ -108,3 +112,51 @@ def comp_different_agents():
     return
 
 # comp_different_agents()
+def single_visualize():
+    AGENT_TYPE = "reinforcement"
+    test_times = 100
+    FROG_OF_WAR = False
+    train_times = 6000
+    l_rate = 0.1
+    d_factor = 0.99
+    expl = 1
+    display_times = 5
+
+    env = gym.make("Taxi-v3")
+    agent = main.Agent.ReinforcementAgent(env=env,
+                    learning_rate=l_rate, discount_factor=d_factor, explore=expl)
+    scores = main.single_test(AGENT_TYPE,agent,True,train_times,test_times)
+    avg_score = np.mean(scores)
+    print("scores: ", scores)
+    print("average score: ", avg_score)
+
+
+    env = gym.make("Taxi-v3", render_mode="human")
+    # display
+    print("-----display-----")
+    for _ in range(display_times):
+        observation, info = env.reset()
+        observation = list(env.decode(observation))
+        terminated, truncated = False, False
+        if FROG_OF_WAR:
+            observation = agent.AddFrogToObs(env, observation)  
+        rewards = 0
+        for s in range(30):
+            action = agent.get_best_action(observation)
+            new_observation, reward, terminated, truncated, info = env.step(action)
+            new_observation = list(env.decode(new_observation))
+            rewards += reward
+            # env.render()
+            observation = new_observation
+
+            if terminated or truncated:
+                break
+
+    env.close()
+    return
+
+if True:
+    # single_visualize()
+    # comp_different_agents()
+    # single_rein_tests()
+    show_performance()
